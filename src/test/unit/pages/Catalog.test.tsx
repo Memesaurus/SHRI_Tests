7517eaf6b4ba9e6e599dbf45342537393d3b0c0e
Memesaurus <Greggrr1702@gmail.com>
2023-07-02 16:34:47 +0300

    catalog test fix

--- src/test/unit/pages/Catalog.test.tsx
+++ src/test/unit/pages/Catalog.test.tsx
@@ -7,21 +7,28 @@ import { ExampleApi } from "../../../client/api";
 
 describe("Catalog", () => {
   it("корректно отображает данные, которые фетчит с апи", async () => {
-    const mockGetProducts = jest.fn().mockResolvedValue({data: [
-        { id: 1, name: "name1", price: 123 },
-        { id: 2, name: "name2", price: 321 },
-      ]});
+    const actual = [
+      { id: 1, name: "name1", price: 123 },
+      { id: 2, name: "name2", price: 321 },
+    ];
+    const mockGetProducts = jest.fn().mockResolvedValue({ data: actual });
     const stubApi = new ExampleApi("/");
     stubApi.getProducts = mockGetProducts;
     const store = initStubStore(null, stubApi);
     const render = renderWithStubs(<Catalog />, store);
 
-
     await waitFor(() => {
-        expect(mockGetProducts).toBeCalled();
-        const actualItems = render.getAllByTestId("catalogItem");
+      expect(mockGetProducts).toBeCalled();
+      const actualItems = render.getAllByTestId("catalogItem");
 
-        expect(actualItems.length).toBe(2);
-    })
+      expect(actualItems.length).toBe(2);
+
+      for (const i of [0, 1]) {
+        const name = actualItems[i].querySelector("h5").textContent;
+        const price = actualItems[i].querySelector("p").textContent;
+
+        expect([name, price]).toEqual([actual[i].name, "$" + actual[i].price]);
+      }
+    });
   });
 });
