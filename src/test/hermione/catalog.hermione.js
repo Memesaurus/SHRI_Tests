f2f5f9f61779c0367a579177ea0906a019a53140
Memesaurus <Greggrr1702@gmail.com>
2023-07-02 14:37:15 +0300

    bugid1 tests

--- src/test/hermione/catalog.hermione.js
+++ src/test/hermione/catalog.hermione.js
@@ -1,3 +1,4 @@
+const { assert } = require("chai");
 const {
   mockCatalogRequest,
   cleanUpCart,
@@ -7,6 +8,8 @@ const {
 describe("catalog", async function () {
   afterEach(async function ({ browser }) {
     cleanUpCart(browser);
+    await browser.mockRestoreAll();
+    await browser.refresh();
   });
 
   it("верстка адаптируется под ширину экрана", async function ({ browser }) {
@@ -19,6 +22,18 @@ describe("catalog", async function () {
     await browser.setWindowSize(800, 5000);
 
     await browser.assertView("catalog-small", ".Catalog");
+  });
+
+  //тест написал после того, как почекал багайдишки, поэтому по хорошему вот этот тест можно не засчитывать
+  it("данные отображаются корректно", async function ({ browser }) {
+    await urlWithBugId("http://localhost:3000/hw/store/catalog", browser);
+
+    const card = await browser.$(".ProductItem");
+    const h5 = await card.$("h5").getText();
+    const p = await card.$("p").getText();
+
+    assert.isNotEmpty(h5, "h5 is empty");
+    assert.isNotEmpty(p, "p is empty");
   });
 
   it("если элемент в корзине, отображает надпись", async function ({
--- src/test/hermione/details.hermione.js
+++ src/test/hermione/details.hermione.js
@@ -1,3 +1,4 @@
+const { assert } = require("chai");
 const {
   mockDetailsRequest,
   cleanUpCart,
@@ -7,6 +8,8 @@ const {
 describe("details", async function () {
   afterEach(async function ({ browser }) {
     await cleanUpCart(browser);
+    await browser.mockRestoreAll();
+    await browser.refresh();
   });
 
   it("верстка адаптируется под ширину экрана", async function ({ browser }) {
@@ -18,6 +21,22 @@ describe("details", async function () {
     await browser.setWindowSize(570, 1080);
 
     await browser.assertView("details-small", ".Product");
+  });
+
+  //тест написал после того, как почекал багайдишки, поэтому по хорошему вот этот тест можно не засчитывать
+  it("данные отображаются корректно", async function ({ browser }) {
+    await urlWithBugId("http://localhost:3000/hw/store/catalog/0", browser);
+
+    const card = await browser.$(".Product");
+    const h1 = await card.$("h1").getText();
+    const [description, price] = await card.$$("p").map((p) => p.getText());
+    const [color, material] = await card.$$("dd").map((dd) => dd.getText());
+
+    assert.isNotEmpty(h1, "h1 is empty");
+    assert.isNotEmpty(description, "description is empty");
+    assert.isNotEmpty(price.slice(1), "price is empty");
+    assert.isNotEmpty(color, "color is empty");
+    assert.isNotEmpty(material, "material is empty");
   });
 
   it("отображает надпись, если товар в корзине", async function ({ browser }) {
