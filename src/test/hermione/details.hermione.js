00189a23e56faf787545edab9ba419e9fc5d1d6e
Memesaurus <Greggrr1702@gmail.com>
2023-07-02 15:47:42 +0300

    caught bugid2

--- src/test/hermione/details.hermione.js
+++ src/test/hermione/details.hermione.js
@@ -24,18 +24,20 @@ describe("details", async function () {
 
   //тест написал после того, как почекал багайдишки, поэтому по хорошему вот этот тест можно не засчитывать
   it("данные отображаются корректно", async function ({ browser }) {
-    await urlWithBugId("http://localhost:3000/hw/store/catalog/0", browser);
+    for (const i of [0, 1]) {
+      await urlWithBugId("http://localhost:3000/hw/store/catalog/" + i, browser);
 
-    const card = await browser.$(".Product");
-    const h1 = await card.$("h1").getText();
-    const [description, price] = await card.$$("p").map((p) => p.getText());
-    const [color, material] = await card.$$("dd").map((dd) => dd.getText());
+      const card = await browser.$(".Product");
+      const h1 = await card.$("h1").getText();
+      const [description, price] = await card.$$("p").map((p) => p.getText());
+      const [color, material] = await card.$$("dd").map((dd) => dd.getText());
 
-    assert.isNotEmpty(h1, "h1 is empty");
-    assert.isNotEmpty(description, "description is empty");
-    assert.isNotEmpty(price.slice(1), "price is empty");
-    assert.isNotEmpty(color, "color is empty");
-    assert.isNotEmpty(material, "material is empty");
+      assert.isNotEmpty(h1, "h1 is empty for i " + i);
+      assert.isNotEmpty(description, "description is empty for i " + i);
+      assert.isNotEmpty(price.slice(1), "price is empty for i " + i);
+      assert.isNotEmpty(color, "color is empty for i " + i);
+      assert.isNotEmpty(material, "material is empty for i " + i);
+    }
   });
 
   it("отображает надпись, если товар в корзине", async function ({ browser }) {
